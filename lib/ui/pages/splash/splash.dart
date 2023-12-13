dca0a457abdf353e879b6a177dceb43d7bcc204e
Adrián Brito Pacheco <adrbrpa1988@gmail.com>
2023-12-13 17:30:11 +0000

    ⚡ Updated splash screen design & functionality

--- lib/ui/pages/splash/splash.dart
+++ lib/ui/pages/splash/splash.dart
@@ -1,14 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class SplashScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/constants/images.dart';
+import 'package:flutter_pokedex/routes.dart';
+
+class SplashScreen extends StatefulWidget {
 
   const SplashScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _SplashScreenState();
+
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  static const double _splashIconSize = 50;
+
+  @override
+  void initState() {
+    scheduleMicrotask(() async {
+      await AppImages.precacheAssets(context);
+      await Future.delayed(const Duration(milliseconds: 400));
+      await AppNavigator.replaceWith(Routes.home);
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Center(
-        child: Text('SplashScreen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image(
+              image: AppImages.pikachuLoader,
+              width: _splashIconSize,
+              height: _splashIconSize,
+              fit: BoxFit.contain,
+            ),
+            Text(
+              'Pokedex',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w600,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
